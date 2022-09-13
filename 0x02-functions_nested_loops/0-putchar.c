fc362976587aebdfd236a083c573f2102e196f9e
Emmanuel <ikezahuemma@gmail.com>
2022-09-13 06:53:23 -0700

    fix 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,23 @@
 #include "main.h"
 
 /**
- * main - Prints _putchar followed by a newline
- *
- * Return: Always 0.
- */
+* main - prints _putchar
+*
+* Description: prints _putchar
+*
+* Return: 0 (Success)
+*/
+
 int main(void)
 {
-	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
-	int count, sz;
+char text[9] = "_putchar";
+int i = 0;
 
-	sz = sizeof(str) / sizeof(int);
-	for (count = 0; count < sz; count++)
-	{
-		_putchar(str[count]);
-	}
-	_putchar('\n');
-	return (0);
+for (i = 0; i < 8; i++)
+{
+_putchar(text[i]);
+}
+_putchar('\n');
+
+return (0);
 }
